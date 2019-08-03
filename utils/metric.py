cdd4a281b53a4db1f1c87543931ab23149da54ec
xuan-vy-nguyen <xuanvy99@gmail.com>
2019-08-04 01:01:11 +0700

    Add f1_score

--- utils/metric.py
+++ utils/metric.py
@@ -1,5 +1,23 @@
-
+from sklearn.metrics import f1_score
 
 
 def multi_class_F1(y_pred, y_true):
-    return 0
+    """
+    Calculate F1 score for each class
+    Parameter
+        y_true  -   torch.tensor()
+            Groundtruth
+        y_pred  -   torch.tensor()
+            Prediction
+    -------------------------
+    Return
+        list
+            F1 score for each class
+    """
+    f1_out = []
+    gt_np = y_true.to("cpu").numpy()
+    pred_np = (y_pred.to("cpu").numpy() > 0.5) * 1.0
+    assert gt_np.shape == pred_np.shape
+    for i in range(gt_np.shape[1]):
+        f1_out.append(f1_score(gt_np[:,i], pred_np[:,i]))
+    return f1_out
