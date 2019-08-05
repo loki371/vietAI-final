c4c31c983630d234255b69258cd27c0a3e13b1a2
xuan-vy-nguyen <xuanvy99@gmail.com>
2019-08-05 16:36:44 +0700

    debug training greyscale->RGB

--- train.py
+++ train.py
@@ -42,7 +42,6 @@ if __name__ == '__main__':
     dict_config = _choose_optim(dict_config)
     dict_config = _choose_criterion(dict_config)
 
-    model = dict_config['model']
     # augmentation
     transform_augment = None
 
