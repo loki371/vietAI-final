42adf02d00fdd6a6d75dc00dd354c4ada250cda9
xuan-vy-nguyen <xuanvy99@gmail.com>
2019-08-09 20:19:34 +0700

    update bestScore & bestLoss

--- train.py
+++ train.py
@@ -92,12 +92,12 @@ if __name__ == '__main__':
         
         val_loss, val_F1  = evaluate(model, valDataloader, criterion, None, dict_config['val_pfeq'])
 
-        if best_F1_score > val_F1:            
+        if best_F1_score < val_F1:            
             print(f'Updated best F1 from {best_F1_score} to {val_F1}')
             torch.save(model.state_dict(), dict_config['bestF1_path'])
             best_F1_score = val_F1
 
-        if best_val_loss < val_loss:
+        if best_val_loss > val_loss:
             print(f"Update best loss from {best_val_loss} to {val_loss}")
             torch.save(model.state_dict(), dict_config['best_valLoss_path'])
             best_val_loss = val_loss
