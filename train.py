a35e9844a3c98908a4cc4a39dd24310984351a68
xuan-vy-nguyen <xuanvy99@gmail.com>
2019-08-09 23:08:27 +0700

    fix condition (b % dict_config == 0)

--- train.py
+++ train.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
 
             if b % dict_config['train_pfeq'] == 0:
                 print(f'Epoch {e} - [{b} / {len(trainDataloader)}]:\nLoss: {eLoss}')
-                print(f"F1: {emetric_value['F1'] / b + int(b==0)}")
+                print(f"F1: {emetric_value['F1'] / (b + int(b==0))}")
         
         if e % dict_config['save_checkpoint_feq'] == 0:
             torch.save(model.state_dict(), dict_config['checkpoint_path'])
